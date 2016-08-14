2a3fdc3f306af679e0abcda25ed162a651d3ea6d
elfanos <elfanos@gmail.com>
2016-08-14 17:08:19 +0200

    added articles to header

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :photos
   resources :albums
   resources :articles
-  root 'start#index'
+  root 'albums#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
