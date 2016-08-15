4c3b0d4c9031aeda0e2efb88910a6d306d869f2a
elfanos <elfanos@gmail.com>
2016-08-15 20:15:30 +0200

    added front page

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,5 +1,6 @@
 class StartController < ApplicationController
   def index
-
+    # @potrait_photo = Photo.where(picture: "frontbild.jpg");
+    # @cover_photo = Photo.where(picture: "frontcoover.jpg");
   end
 end
