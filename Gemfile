922422214a1a979577f91aadf718038721968277
elfanos <elfanos@gmail.com>
2016-08-15 20:33:32 +0200

    added mail contact information

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'dotenv-rails'
 
 gem "paperclip", "~> 5.0.0"
 
+gem 'mail_form'
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :albums
   resources :articles
   resources :start
+
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+
+  resources "contacts", only: [:new, :create]
+
   root 'start#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
